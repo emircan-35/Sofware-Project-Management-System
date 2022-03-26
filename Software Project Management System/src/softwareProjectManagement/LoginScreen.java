087888afcdeffe78f6a9da28cf206331e0654eb7
gokay2027 <gokaydinc2000@gmail.com>
2022-03-26 21:01:20 +0300

    gradient jar added

--- Software Project Management System/src/softwareProjectManagement/LoginScreen.java
+++ Software Project Management System/src/softwareProjectManagement/LoginScreen.java
@@ -9,14 +9,17 @@ import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import keeptoo.KGradientPanel;
+import javax.swing.JPasswordField;
 
 public class LoginScreen {
 
 	private JFrame frame;
 	private JTextField textField;
-	private JTextField textField_1;
+	private JPasswordField passwordField;
 	
-	private ImageIcon image = new ImageIcon("software.jpeg");
+	
 	
 	/**
 	 * Launch the application.
@@ -46,46 +49,49 @@ public class LoginScreen {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 581, 336);
+		frame.setResizable(false);
+		frame.getContentPane().setBackground(new Color(255, 255, 0));
+		frame.setBounds(100, 100, 369, 336);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Username:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblNewLabel.setBounds(26, 114, 131, 31);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Password:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_1.setBounds(25, 163, 114, 31);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(25, 165, 114, 31);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(25, 143, 208, 24);
+		textField.setBounds(25, 143, 208, 27);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(25, 194, 208, 24);
-		frame.getContentPane().add(textField_1);
 		
 		JButton btnNewButton = new JButton("Login");
 		btnNewButton.setBounds(26, 229, 131, 39);
 		frame.getContentPane().add(btnNewButton);
 		
 		JLabel lblNewLabel_2 = new JLabel("Software Development");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNewLabel_2.setBounds(35, 23, 414, 68);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel(" Management System");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_3.setBounds(37, 72, 208, 31);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_3.setBounds(37, 72, 252, 31);
 		frame.getContentPane().add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel(image);
-		lblNewLabel_4.setBounds(567, 0, -281, 297);
-		frame.getContentPane().add(lblNewLabel_4);
+		passwordField = new JPasswordField();
+		passwordField.setBounds(26, 191, 207, 27);
+		frame.getContentPane().add(passwordField);
+		
+		KGradientPanel gradientPanel = new KGradientPanel();
+		gradientPanel.setBounds(0, 0, 353, 297);
+		frame.getContentPane().add(gradientPanel);
+		
+		
 	}
 }
