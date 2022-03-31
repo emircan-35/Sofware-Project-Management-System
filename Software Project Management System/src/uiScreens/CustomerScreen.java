b2a310a4c4c2306714a45224a5e6d380891354d0
gokay2027 <gokaydinc2000@gmail.com>
2022-03-31 14:19:37 +0300

    Customer and Meets screen(for manager) have been added

---
+++ Software Project Management System/src/uiScreens/CustomerScreen.java
@@ -0,0 +1,120 @@
+package uiScreens;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import keeptoo.KGradientPanel;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class CustomerScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CustomerScreen window = new CustomerScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CustomerScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 540, 433);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		KGradientPanel gradientPanel = new KGradientPanel();
+		gradientPanel.setBounds(0, 282, 524, 120);
+		frame.getContentPane().add(gradientPanel);
+		gradientPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("MENU");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 23));
+		lblNewLabel.setBounds(10, 11, 106, 32);
+		gradientPanel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Project Order");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 9));
+		btnNewButton.setBounds(10, 54, 106, 32);
+		gradientPanel.add(btnNewButton);
+		
+		JButton btnProjectStatus = new JButton("Project Status");
+		btnProjectStatus.setFont(new Font("Tahoma", Font.BOLD, 9));
+		btnProjectStatus.setBounds(126, 54, 106, 32);
+		gradientPanel.add(btnProjectStatus);
+		
+		JLabel lblNewLabel_1 = new JLabel("Personal \r");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblNewLabel_1.setBounds(10, 0, 339, 75);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("\nInformation");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblNewLabel_2.setBounds(10, 54, 284, 47);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Title:");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1_1.setBounds(10, 102, 103, 34);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_3.setBounds(67, 113, 83, 14);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Name:");
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1_1_1.setBounds(10, 147, 103, 34);
+		frame.getContentPane().add(lblNewLabel_1_1_1);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("New label");
+		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_3_1.setBounds(80, 161, 83, 14);
+		frame.getContentPane().add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Surname:");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1_2.setBounds(10, 192, 103, 34);
+		frame.getContentPane().add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_3_2 = new JLabel("New label");
+		lblNewLabel_3_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_3_2.setBounds(114, 206, 83, 14);
+		frame.getContentPane().add(lblNewLabel_3_2);
+		
+		JLabel lblNewLabel_1_6 = new JLabel("Phone Number:");
+		lblNewLabel_1_6.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1_6.setBounds(10, 237, 169, 34);
+		frame.getContentPane().add(lblNewLabel_1_6);
+		
+		JLabel lblNewLabel_3_6 = new JLabel("New label");
+		lblNewLabel_3_6.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_3_6.setBounds(178, 248, 83, 14);
+		frame.getContentPane().add(lblNewLabel_3_6);
+	}
+}
---
+++ Software Project Management System/src/uiScreens/MeetsScreen.java
@@ -0,0 +1,132 @@
+package uiScreens;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import keeptoo.KGradientPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class MeetsScreen {
+
+	private JFrame frame;
+	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MeetsScreen window = new MeetsScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MeetsScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 693, 577);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		KGradientPanel gradientPanel = new KGradientPanel();
+		gradientPanel.setBounds(188, 0, 499, 538);
+		frame.getContentPane().add(gradientPanel);
+		gradientPanel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 467, 516);
+		gradientPanel.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Meet Name", "Description", "Time"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JLabel lblNewLabel = new JLabel("Meets");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblNewLabel.setBounds(29, 19, 129, 62);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Meet Name:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(10, 92, 129, 24);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 120, 168, 24);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Description:");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1_1.setBounds(10, 161, 129, 24);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(10, 189, 168, 24);
+		frame.getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Time: (TextBox de\u011Fi\u015Fecek)");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1_2.setBounds(10, 234, 181, 24);
+		frame.getContentPane().add(lblNewLabel_1_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(10, 262, 168, 24);
+		frame.getContentPane().add(textField_2);
+		
+		JLabel lblNewLabel_2 = new JLabel("Project:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 19));
+		lblNewLabel_2.setBounds(10, 312, 129, 38);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Project Name");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_3.setBounds(89, 322, 98, 22);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JButton btnNewButton = new JButton("Arrange Meet");
+		btnNewButton.setBounds(10, 387, 168, 38);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnCancelMeet = new JButton("Cancel Meet");
+		btnCancelMeet.setBounds(10, 436, 168, 38);
+		frame.getContentPane().add(btnCancelMeet);
+		
+	}
+}
